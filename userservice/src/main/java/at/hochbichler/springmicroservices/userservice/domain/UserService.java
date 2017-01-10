04a7f764f28db3156729927ba3ccd8538c877f3a
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2017-01-10 16:26:49 +0100

    Add context path userservice

--- userservice/src/main/java/at/hochbichler/springmicroservices/userservice/domain/UserService.java
+++ userservice/src/main/java/at/hochbichler/springmicroservices/userservice/domain/UserService.java
@@ -3,6 +3,7 @@ package at.hochbichler.springmicroservices.userservice.domain;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by xhocht on 09.01.17.
