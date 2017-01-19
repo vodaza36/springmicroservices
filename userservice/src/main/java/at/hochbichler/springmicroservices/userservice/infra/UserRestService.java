66993ff4337c7ed1d7c066bfc610fb2b9546f3df
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2017-01-19 13:24:08 +0100

    Add new docker phases to parent pom

--- userservice/src/main/java/at/hochbichler/springmicroservices/userservice/infra/UserRestService.java
+++ userservice/src/main/java/at/hochbichler/springmicroservices/userservice/infra/UserRestService.java
@@ -4,12 +4,11 @@ import at.hochbichler.springmicroservices.userservice.domain.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by xhocht on 09.01.17.
  */
-@RepositoryRestResource(itemResourceRel = "User", path = "user")
+@RepositoryRestResource(collectionResourceRel = "users", path = "user")
 public interface UserRestService extends PagingAndSortingRepository<User, Long> {
     User findByEmail(@Param("email") String email);
 }
